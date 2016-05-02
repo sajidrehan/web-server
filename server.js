0ff087ad636ed6fd922f485407d48f4fb6fb51dc
Rehan Sajid <rehansajid.dev@gmail.com>
2016-05-02 09:31:20 +0500

    Add exclamation marks

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ var middleware = {
 app.use(middleware.logger);
 
 app.get('/about', middleware.requireAuthentication, function (req, res) {
-	res.send('About Us!');
+	res.send('About Us!!!');
 });
 
 app.use(express.static(__dirname + "/public"));
