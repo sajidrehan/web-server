696ff8b4d54ad3ef55b0f57d688942228884b335
Rehan Sajid <rehansajid.dev@gmail.com>
2016-05-02 09:26:32 +0500

    Initialize repo

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+// app.get('/', function (req, res) {
+// 	res.send('yo baby!');
+
+
+// });
+
+var middleware = {
+	requireAuthentication: function (req, res, next) {
+		console.log('Private route hit!');
+		next();
+	},
+	logger: function (req, res, next) {
+		console.log("Request : " + ' ' + new Date().toString()+ ' ' +req.method + ' ' + req.originalUrl + '!');
+		next();
+
+	}
+
+
+};
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function (req, res) {
+	res.send('About Us!');
+});
+
+app.use(express.static(__dirname + "/public"));
+
+app.listen(PORT, function () {
+	console.log('server started at port '+ PORT);
+});
